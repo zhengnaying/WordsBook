2d32c3dc2e8bf5ab84ec550dcfb087dbd497171f
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-05 09:08:18 +0800

    1

--- app/src/main/java/com/example/wordsbook/Words_DB_History.java
+++ app/src/main/java/com/example/wordsbook/Words_DB_History.java
@@ -8,15 +8,14 @@ import android.util.Log;
 import androidx.annotation.Nullable;
 
 public class Words_DB_History extends SQLiteOpenHelper {
-    private String CREATE_AGO_TABLE="create table words_ago(_id integer primary key autoincrement,word_ago varchar(200))";
+    private String CREATE_HISTORY_TABLE="create table words_history(_id integer primary key autoincrement,word_history varchar(200))";
     public Words_DB_History(@Nullable Context context) {
         super(context,"record_db",null,2);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(CREATE_AGO_TABLE);
-        Log.d("#DB","WordAgo");
+        db.execSQL(CREATE_HISTORY_TABLE);
     }
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
--- app/src/main/java/com/example/wordsbook/left_Fragment.java
+++ app/src/main/java/com/example/wordsbook/left_Fragment.java
@@ -1,64 +1,86 @@
 package com.example.wordsbook;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.speech.tts.TextToSpeech;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link left_Fragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class left_Fragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+public class left_Fragment extends Fragment implements TextToSpeech.OnInitListener{
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public left_Fragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment left_Fragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static left_Fragment newInstance(String param1, String param2) {
-        left_Fragment fragment = new left_Fragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
+    EditText word_input;
+    TextView find,history,clear,meaning,result;
+    view_History list_view;
+    Words_DB_Helper db_helper;
+    Words_DB_History db_history;
+    SQLiteDatabase dictionary_db, history_db;
+    Cursor cursor;
+    SimpleCursorAdapter adapter;
+    String del_word="";
+    String change_word = "";
+    String sample = "";
+    View view;
+    private TextToSpeech textToSpeech;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_left_, container, false);
+        view= inflater.inflate(R.layout.fragment_left_, container, false);
+        textToSpeech = new TextToSpeech(getActivity(),this);
+        initView();
+        initData();
+        initListener();
+        list_view.setOnCreateContextMenuListener(new View.OnCreateContextMenuListener() {
+            @Override
+            public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+                menu.setHeaderTitle("选择操作");
+                menu.add(0,0,0,"删除该条");
+                menu.add(0,1,0,"修改该条");
+            }
+        });
+        return view;
+    }
+
+    private void initListener() {
+    }
+
+    //获取历史记录
+    private void initData() {
+        db_helper = new Words_DB_Helper(this.getActivity());
+        db_history = new Words_DB_History(this.getActivity());
+        cursor = db_history.getReadableDatabase().rawQuery("select * from words_history ", null);
+
+    }
+
+    private void initView() {
+        word_input = view.findViewById(R.id.word);
+        find = view.findViewById(R.id.find);
+        history = view.findViewById(R.id.history);
+        clear = view.findViewById(R.id.clear);
+        list_view = view.findViewById(R.id.listView);
+        meaning=view.findViewById(R.id.meaning);
+        result=view.findViewById(R.id.result);
+    }
+
+    @Override
+    public void onInit(int status) {
+
     }
 }
