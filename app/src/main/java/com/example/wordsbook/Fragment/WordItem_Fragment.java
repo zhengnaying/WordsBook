c3cfcefd077abfefc14d4f2d079b5b00d1bfa448
zhengnaying <55484284+zhengnaying@users.noreply.github.com>
2020-10-06 10:38:53 +0800

    1

--- app/src/main/java/com/example/wordsbook/Fragment/WordItem_Fragment.java
+++ app/src/main/java/com/example/wordsbook/Fragment/WordItem_Fragment.java
@@ -7,10 +7,14 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.wordsbook.DB.WordsDB;
 import com.example.wordsbook.R;
@@ -43,6 +47,8 @@ public class WordItem_Fragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //刷新单词列表
+        refreshWordsList();
 
     }
 
@@ -55,6 +61,7 @@ public class WordItem_Fragment extends ListFragment {
         // Inflate the layout for this fragment
         View view = super.onCreateView(inflater,container,savedInstanceState);
         //为列表注册上下文菜单
+        assert view != null;
         ListView mListview = (ListView)view.findViewById(android.R.id.list);
         registerForContextMenu(mListview);
         return view;
@@ -64,6 +71,22 @@ public class WordItem_Fragment extends ListFragment {
     public void onDetach() {
         super.onDetach();
         mListener =null;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        TextView textId = null;
+        TextView textWord = null;
+        TextView textMeaning = null;
+        TextView textSample = null;
+
+        AdapterView.AdapterContextMenuInfo info = null;
+        View itemView = null;
+
+        switch (item.getItemId()){
+
+        }
+
     }
 
     //更新单词列表，从数据库中找到所有单词，然后在列表中显示出来
@@ -76,12 +99,41 @@ public class WordItem_Fragment extends ListFragment {
         }
     }
 
+    //更新单词列表，从数据库中找到同strWord向匹配的单词，然后在列表中显示出来
+    public void refreshWordsList(String strWord){
+        WordsDB wordsDB = WordsDB.getWordsDB();
+        if(wordsDB!= null){
+            ArrayList<Map<String,String>> items = wordsDB.SearchUseSql(strWord);
+            if(items.size()>0){
+                SimpleAdapter adapter = new SimpleAdapter(getActivity(),items,R.layout.item,new String[]{Words.Word._ID,Words.Word.COLUMN_NAME_WORD},
+                        new int[]{R.id.textId,R.id.textViewWord});
+                setListAdapter(adapter);
+            }else
+                Toast.makeText(getActivity(),"Not found", Toast.LENGTH_LONG).show();
+
+        }
+    }
+
+
+
+    public void onListItemClick(ListView l,View v ,int position,long id){
+        super.onListItemClick(l,v,position,id);
+
+        if(mListener!= null){
+            //通知Fragment所在的Activity，用户单击了列表的position项
+            TextView textView = (TextView)v.findViewById(R.id.textId);
+            if(textView!= null){
+                //将单词ID传过去
+                mListener.onWordItemClick(textView.getText().toString());
+            }
+        }
+    }
     /**
  * Fragment所在的Activity必须实现该接口，通过该接口Fragment和Activity可以进行通信
  */
 
     public interface OnFragmentInteractionListener{
-        public void onWordItemInteractionListener(String id);
+        public void onWordItemClick(String id);
         public void onDeleteDialog(String strId);
         public void onUpdateDialog(String strId);
     }
--- app/src/main/java/com/example/wordsbook/WordDetailActivity.java
+++ app/src/main/java/com/example/wordsbook/WordDetailActivity.java
@@ -1,6 +1,32 @@
 package com.example.wordsbook;
 
 import android.app.Activity;
+import android.content.res.Configuration;
+import android.net.Uri;
+import android.os.Bundle;
 
-public class WordDetailActivity extends Activity {
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.wordsbook.Fragment.WordDetail_Fragment;
+
+public class WordDetailActivity extends AppCompatActivity implements WordDetail_Fragment.OnFragmentInteractionListener {
+
+    @Override
+    public void onWordDetailClick(Uri uri) {
+
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //如果是横屏的话直接退出
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            finish();
+            return;
+        }
+
+
+    }
 }
